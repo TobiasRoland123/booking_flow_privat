6804debb86fcbf8b94781d450de909e096f29521
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-06-13 11:21:11 +0200

    spotAmount now updates when TicketAmount changes

--- src/components/AreaList.js
+++ src/components/AreaList.js
@@ -1,11 +1,16 @@
 import { useState, useEffect, useContext } from "react";
 import { BookingInformation } from "@/pages/_app";
 import { AreaListItem } from "./AreaListItem";
+import { Button } from "@mui/material";
 
 export function AreaList(props) {
   const [chosenArea, setChosenArea] = useState("");
   const [bookingDetails, setBookingDetails] = useContext(BookingInformation);
   const [spotAmount, setSpotAmount] = useState(0);
+  const [bookedArea, setBookedArea] = useState("");
+
+  // This variable is only used, to make spotAmount update when ticketAmount is changed
+  const [lastChosenArea, setLastChosenArea] = useState("");
   // state for modal
   const [open, setOpen] = useState(false);
 
@@ -15,7 +20,8 @@ export function AreaList(props) {
 
   /* This function checks if there is enough available spots for the chosen amount of tickets  */
   function checkTicketAndArea(clickedArea) {
-    // checks if you want to have a tent spot for each ticket, or if you are willing to share tent.
+    console.log("clickedArea", clickedArea);
+    setLastChosenArea(clickedArea); // checks if you want to have a tent spot for each ticket, or if you are willing to share tent.
     if (bookingDetails.oneTentForEach === true) {
       // Checks if there is enough spots available and sets spotAmount
       bookingDetails.ticketAmount <= clickedArea.available
@@ -40,6 +46,13 @@ export function AreaList(props) {
     setSpotAmount(spots);
   }
 
+  //This useEffect is used to update spotAmount every time that ticketAmount is updated.
+  useEffect(() => {
+    /*By default lasChosenArea is ""(empty string), so if it has been set to something else, then
+    call checkTicketAndArea with lasChosenArea as an argument*/
+    lastChosenArea !== "" ? checkTicketAndArea(lastChosenArea) : console.log("lastChosenArea, has not been set");
+  }, [bookingDetails.ticketAmount]);
+
   useEffect(() => {
     updateBookingInformation();
   }, [spotAmount, /* bookingDetails.ticketAmount,*/ chosenArea]);
@@ -47,7 +60,7 @@ export function AreaList(props) {
   // This function updates the bookingInformation, so that it  also contains the clicked area and amount of spots to reserve
   function updateBookingInformation() {
     console.log(`updateBookingInformation called`);
-    console.log(props.chosenArea);
+
     setBookingDetails((prev) => ({
       ...prev,
       area: chosenArea,
@@ -73,6 +86,8 @@ export function AreaList(props) {
           ))}
         </section>
       </div>
+
+      <Button onClick={() => console.log("lastChosenArea", lastChosenArea)}>log lastChosenArea</Button>
     </>
   );
 }
