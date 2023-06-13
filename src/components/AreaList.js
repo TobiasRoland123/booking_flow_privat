ad1efc66aed39356e273f338a25cbfa0afa10d44
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-06-13 11:37:36 +0200

    modal has been moved to the AreaList component

--- src/components/AreaList.js
+++ src/components/AreaList.js
@@ -3,6 +3,14 @@ import { BookingInformation } from "@/pages/_app";
 import { AreaListItem } from "./AreaListItem";
 import { Button } from "@mui/material";
 
+// imported for modal.......
+import Modal from "@mui/material/Modal";
+import Backdrop from "@mui/material/Backdrop";
+import Fade from "@mui/material/Fade";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+// ..........
+
 export function AreaList(props) {
   const [chosenArea, setChosenArea] = useState("");
   const [bookingDetails, setBookingDetails] = useContext(BookingInformation);
@@ -17,6 +25,19 @@ export function AreaList(props) {
   // creates functions to handle modal
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  // styling for modal
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "#000",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   /* This function checks if there is enough available spots for the chosen amount of tickets  */
   function checkTicketAndArea(clickedArea) {
@@ -70,6 +91,50 @@ export function AreaList(props) {
 
   return (
     <>
+      {/* modal from mui */}
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography
+              className="font-sans"
+              id="transition-modal-title"
+              variant="h6"
+              component="h2"
+            >
+              Not enough space on {lastChosenArea.area}
+            </Typography>
+            <Typography
+              className="font-sans"
+              id="transition-modal-description"
+              sx={{
+                mt: 2,
+              }}
+            >
+              Choose another area or adjust the amount of tickets to match the available number of spots.
+            </Typography>
+            <div className="mt-10 flex justify-center">
+              <Button
+                className=" mb-10 h-10 gap-5 place-self-center rounded-none border-2 border-solid border-color-yellow px-6 font-sans font-semibold text-color-yellow hover:bg-color-yellow hover:text-color-black "
+                onClick={handleClose}
+              >
+                <span className="pt-1">Close</span>
+              </Button>
+            </div>
+          </Box>
+        </Fade>
+      </Modal>
       <div className="mx-3 md:mx-0 ">
         <section className="flex flex-col gap-2 sm:grid sm:grid-cols-2 ">
           {/* maps through area, and returns a component for each item.  */}
@@ -86,7 +151,6 @@ export function AreaList(props) {
           ))}
         </section>
       </div>
-
       <Button onClick={() => console.log("lastChosenArea", lastChosenArea)}>log lastChosenArea</Button>
     </>
   );
